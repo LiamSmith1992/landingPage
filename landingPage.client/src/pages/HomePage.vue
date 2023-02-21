c6aafe6bb3b1d3ace9a3c35a6dfe7e386eed0b75
LiamSmith1992 <liamj.smith@hotmail.com>
2023-02-21 14:33:34 -0700

    init

---
+++ landingPage.client/src/pages/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container-fluid p-0">
+    <section>
+
+      <img class="big-img"
+        src="https://images.unsplash.com/photo-1675981004510-4ec798f42006?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2N3xDRHd1d1hKQWJFd3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+        alt="">
+      <div class="text-center">
+
+        <H1>LIAM SMITH</H1>
+        <H3>Software Developer</H3>
+        <h4>Loves to type</h4>
+        <h5>click clack click clack on the keys makes me so happy i love it!</h5>
+        <h5>Hire me today! </h5>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.big-img {
+  height: 100vh;
+  width: 100%;
+  object-fit: cover;
+}
+</style>
